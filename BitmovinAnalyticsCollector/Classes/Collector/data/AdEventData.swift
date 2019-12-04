e7f501df5f190a3ee264a5839f8891c0e0e9af1b
Thomas Sablattnig <thomas.sablattnig@bitmovin.com>
2019-12-04 08:27:21 +0100

    added AdSample to AdEventData mapping Sorted fields

--- BitmovinAnalyticsCollector/Classes/Collector/data/AdEventData.swift
+++ BitmovinAnalyticsCollector/Classes/Collector/data/AdEventData.swift
@@ -7,9 +7,8 @@
 
 import Foundation
 public class AdEventData: Codable {
-    var videoImpressionId: String?
-    var userAgent: String?
-    var language: String?
+    //  EventData data
+    var audioCodec : String?
     var cdnProvider : String?
     var customData1: String?
     var customData2 : String?
@@ -22,43 +21,119 @@ public class AdEventData: Codable {
     var domain : String?
     var experimentName: String?
     var key : String?
+    var language: String?
     var path : String?
+    var platform : String?
     var player: String?
     var playerKey: String?
     var playerTech: String?
     var screenHeight: Int?
     var screenWidth:Int?
-    var version : String?
+    var userAgent: String?
     var userId : String?
+    var version : String?
+    var videoCodec: String?
     var videoId : String?
+    var videoImpressionId: String?
     var videoTitle : String?
     var videoWindowHeight:Int?
     var videoWindowWidth:Int?
-    var platform : String?
-    var audioCodec : String?
-    var videoCodec: String?
     
+    //  AdBreak data
+    var adFallbackIndex: Double = 0
     var adIdPlayer: String?
-    var adPosition: String?
+    var adIsPersistent: Bool?
     var adOffset: String?
-    var adScheduleTime: Double?
-    var adReplaceContentDuration: Double?
+    var adPosition: String?
     var adPreloadOffset: Double?
+    var adReplaceContentDuration: Double?
+    var adScheduleTime: Double?
     var adTagType: String?
     var adTagUrl: String?
     var adTagServer: String?
     var adTagPath: String?
-    var adIsPersistent: Bool?
-    var adFallbackIndex: Double = 0
+    
+    //  AdSample data
+    //    var timeHovered: Double?
+    //    var timeInViewport: Double?
+    //    var timeUntilHover: Double?
+    var adPodPosition: Int?
+    var adStartupTime: Double?
+    var clicked: Double = 0
+    var clickPercentage: Int?
+    var clickPosition: Double?
+    var closed: Double = 0
+    var closePercentage: Int?
+    var closePosition: Double?
+    var completed: Double = 0
+    var errorCode: Int?
+    var errorData: String?
+    var errorMessage: String?
+    var errorPercentage: Int?
+    var errorPosition: Double?
+    var exitPosition: Double?
+    var midpoint: Double?
+    var playPercentage: Int?
+    var quartile1: Double = 0
+    var quartile3: Double = 0
+    var skipped: Double = 0
+    var skipPercentage: Int?
+    var skipPosition: Double?
+    var started: Double = 0
+    var timeFromContent: Double?
+    var timePlayed: Double?
+    var timeToContent: Double?
+    
+    // Ad data
+    var adClickThroughUrl: String?
+    var adDescription: String?
+    var adDuration: Double?
+    var adId: String?
+    var adPlaybackHeight: Int?
+    var adPlaybackWidth: Int?
+    var adSkippable: Bool?
+    var adSkippableAfter: Double?
+    var adSystem: String?
+    var adTitle: String?
+    var advertiserName: String?
+    var apiFramework: String?
+    var creativeAdId: String?
+    var creativeId: String?
+    var dealId: String?
+    var isLinear: Bool = false
+    var mimeType: String?
+    var mediaPath: String?
+    var mediaServer: String?
+    var mediaUrl: String?
+    var minSuggestedDuration: Double?
+    var streamFormat: String?
+    var surveyUrl: String?
+    var universalAdIdRegistry: String?
+    var universalAdIdValue: String?
+    var videoBitrate: Int?
+    var wrapperAdsCount: Int?
+    
+    var manifestDownloadTime: Double?
+    var analyticsVersion: String?
+    var adModule: String?
+    var adModuleVersion: String?
+    var playerStartuptime: Int?
+    var autoplay: Bool?
+    var time: Double?
+    var adImpressionId: String?
     
     init(){
     }
     
+    init(eventData: EventData, adBreak: AdBreak, adSample: AdSample){
+        setEventData(eventData: eventData);
+        setAdBreak(adBreak: adBreak)
+        setAdSample(adSample: adSample)
+    }
+    
     
     public func setEventData(eventData: EventData){
-        self.videoImpressionId = eventData.impressionId
-        self.userAgent = eventData.userAgent
-        self.language = eventData.language
+        self.audioCodec = eventData.audioCodec
         self.cdnProvider = eventData.cdnProvider
         self.customData1 = eventData.customData1
         self.customData2 = eventData.customData2
@@ -71,35 +146,94 @@ public class AdEventData: Codable {
         self.domain = eventData.domain
         self.experimentName = eventData.experimentName
         self.key = eventData.key
+        self.language = eventData.language
         self.path = eventData.path
+        self.platform = eventData.platform
         self.player = eventData.player
         self.playerKey = eventData.playerKey
         self.playerTech = eventData.playerTech
         self.screenHeight = eventData.screenHeight
         self.screenWidth = eventData.screenWidth
-        self.version = eventData.version
+        self.userAgent = eventData.userAgent
         self.userId = eventData.userId
+        self.version = eventData.version
+        self.videoCodec = eventData.videoCodec
         self.videoId = eventData.videoId
         self.videoTitle = eventData.videoTitle
         self.videoWindowHeight = eventData.videoWindowHeight
         self.videoWindowWidth = eventData.videoWindowWidth
-        self.platform = eventData.platform
-        self.audioCodec = eventData.audioCodec
-        self.videoCodec = eventData.videoCodec
+        self.videoImpressionId = eventData.impressionId
     }
     
     public func setAdBreak(adBreak: AdBreak){
+        self.adFallbackIndex = adBreak.fallbackIndex
+        self.adIsPersistent = adBreak.persistent
+        self.adIdPlayer = adBreak.id
         self.adPosition = adBreak.position!.rawValue
         self.adOffset = adBreak.offset
-        self.adScheduleTime = adBreak.scheduleTime
-        self.adReplaceContentDuration = adBreak.replaceContentDuration
         self.adPreloadOffset = adBreak.preloadOffset
+        self.adReplaceContentDuration = adBreak.replaceContentDuration
+        self.adScheduleTime = adBreak.scheduleTime
         (self.adTagServer, self.adTagPath) = Util.getHostNameAndPath(uriString: adBreak.tagUrl)
         self.adTagType = adBreak.tagType!.rawValue
         self.adTagUrl = adBreak.tagUrl
-        self.adIsPersistent = adBreak.persistent
-        self.adIdPlayer = adBreak.id
-        self.adFallbackIndex = adBreak.fallbackIndex
+    }
+    
+    public func setAdSample(adSample: AdSample){
+        self.adPodPosition = adSample.adPodPosition
+        self.adStartupTime = adSample.adStartupTime
+        self.clicked = adSample.clicked
+        self.clickPercentage = adSample.clickPercentage
+        self.clickPosition = adSample.clickPosition
+        self.closed = adSample.closed
+        self.closePercentage = adSample.closePercentage
+        self.closePosition = adSample.closePosition
+        self.completed = adSample.completed
+        self.errorCode = adSample.errorCode
+        self.errorData  = adSample.errorData
+        self.errorMessage = adSample.errorMessage
+        self.errorPercentage = adSample.errorPercentage
+        self.errorPosition = adSample.errorPosition
+        self.exitPosition = adSample.exitPosition
+        self.midpoint = adSample.midpoint
+        self.playPercentage = adSample.playPercentage
+        self.quartile1 = adSample.quartile1
+        self.quartile3 = adSample.quartile3
+        self.skipped = adSample.skipped
+        self.skipPercentage = adSample.skipPercentage
+        self.skipPosition = adSample.skipPosition
+        self.started = adSample.started
+        self.timeFromContent = adSample.timeFromContent
+        self.timePlayed = adSample.timePlayed
+        self.timeToContent = adSample.timeToContent
+    }
+    
+    private func setAd(ad: Ad){
+        self.adClickThroughUrl = ad.clickThroughUrl
+        self.adDescription = ad.description
+        self.adDuration = ad.duration
+        self.adId = ad.id
+        self.adPlaybackHeight = ad.height
+        self.adPlaybackWidth = ad.width
+        self.adSkippable = ad.skippable
+        self.adSkippableAfter = ad.skippableAfter
+        self.adSystem = ad.adSystemName
+        self.adTitle = ad.title
+        self.advertiserName = ad.advertiserName
+        self.apiFramework = ad.apiFramework
+        self.creativeAdId = ad.creativeAdId
+        self.creativeId = ad.creativeId
+        self.dealId = ad.dealId
+        self.isLinear = ad.isLinear
+        (self.mediaServer, self.mediaPath) = Util.getHostNameAndPath(uriString: ad.mediaFileUrl)
+        self.mediaUrl = ad.mediaFileUrl
+        self.minSuggestedDuration = ad.minSuggestedDuration
+        self.streamFormat = ad.mimeType
+        self.surveyUrl = ad.surveyUrl
+        self.universalAdIdRegistry = ad.universalAdIdRegistry
+        self.universalAdIdValue = ad.universalAdIdValue
+        self.videoBitrate = ad.bitrate
+        self.wrapperAdsCount = ad.wrapperAdsCount
     }
     
     public func jsonString() -> String {
