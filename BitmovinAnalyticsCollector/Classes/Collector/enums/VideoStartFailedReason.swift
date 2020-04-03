b1833f9b1313680b98829845d12411e2d23da43e
Thomas Sablattnig <thomas.sablattnig@bitmovin.com>
2020-04-03 06:59:58 +0200

    added videostartFailed and Reason to eventData

---
+++ BitmovinAnalyticsCollector/Classes/Collector/enums/VideoStartFailedReason.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+public struct VideoStartFailedReason {
+    static var pageClosed: String = "PAGE_CLOSED"
+    static var playerError: String = "PLAYER_ERROR"
+    static var timeout: String = "TIMEOUT"
+    static var unknown: String = "UNKNOWN"
+}
