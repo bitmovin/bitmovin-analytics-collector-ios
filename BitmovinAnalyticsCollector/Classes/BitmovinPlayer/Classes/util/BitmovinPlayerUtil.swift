57fe07087291cd74eddef5b28cf60563956193cf
Peter Pfaffeneder <peter.pfaffeneder@gmail.com>
2020-07-01 15:24:11 +0200

    Extracted ms to be a playerUtil function

--- BitmovinAnalyticsCollector/Classes/BitmovinPlayer/Classes/util/BitmovinPlayerUtil.swift
+++ BitmovinAnalyticsCollector/Classes/BitmovinPlayer/Classes/util/BitmovinPlayerUtil.swift
@@ -46,4 +46,12 @@ public class BitmovinPlayerUtil {
                 fatalError()
         }
     }
+
+    static func ms(t: TimeInterval?) -> Int64? {
+        guard let time = t, time > Double(Int64.min) && time < Double(Int64.max) else {
+            return nil
+        }
+        
+        return Int64(time * 1_000)
+    }
 }
