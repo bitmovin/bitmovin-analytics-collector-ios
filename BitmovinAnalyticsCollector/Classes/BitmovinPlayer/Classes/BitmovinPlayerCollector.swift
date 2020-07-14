08b72db18f65d1fc84bbbce999432226cd46a3d0
Roland Griesser <roland.griesser@bitmovin.com>
2020-07-14 14:42:57 +0200

    Added code to check if autoplay is enabled

--- BitmovinAnalyticsCollector/Classes/BitmovinPlayer/Classes/BitmovinPlayerCollector.swift
+++ BitmovinAnalyticsCollector/Classes/BitmovinPlayer/Classes/BitmovinPlayerCollector.swift
@@ -11,9 +11,14 @@ public class BitmovinPlayerCollector : BitmovinAnalyticsInternal {
      * will start monitoring and sending analytics data based on the attached player instance.
      */
     @objc public func attachPlayer(player: BitmovinPlayer) {
-        attach(adapter: BitmovinPlayerAdapter(player: player, config: config, stateMachine: stateMachine))
+        let autoplay = getIsAutoplayEnabled(player.config)
+        attach(adapter: BitmovinPlayerAdapter(player: player, config: config, stateMachine: stateMachine), autoplay: autoplay)
         if (self.adAnalytics != nil) {
             attachAd(adAdapter: BitmovinAdAdapter(bitmovinPlayer: player, adAnalytics: self.adAnalytics!))
         }
+    }
+    
+    func getIsAutoplayEnabled(_ playerConfiguration: PlayerConfiguration) -> Bool {
+        return playerConfiguration.playbackConfiguration.isAutoplayEnabled && playerConfiguration.sourceConfiguration.firstSourceItem != nil
     }
 }
