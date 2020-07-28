711f5ac3e3d449280b9d9e556c44e991c5710af7
Roland Griesser <roland.griesser@bitmovin.com>
2020-07-28 14:27:20 +0200

    Wrong data types

--- BitmovinAnalyticsCollector/Classes/Collector/util/ErrorCode.swift
+++ BitmovinAnalyticsCollector/Classes/Collector/util/ErrorCode.swift
@@ -7,7 +7,7 @@ struct ErrorCode {
     static let ANALYTICS_QUALITY_CHANGE_THRESHOLD_EXCEEDED = ErrorCode(code: 10000, message: "ANALYTICS_QUALITY_CHANGE_THRESHOLD_EXCEEDED")
     static let ANALYTICS_BUFFERING_TIMEOUT_REACHED = ErrorCode(code: 10001, message: "ANALYTICS_BUFFERING_TIMEOUT_REACHED")
     
-    var data: [Int: String] {
+    var data: [String: Any] {
         return [BitmovinAnalyticsInternal.ErrorCodeKey: code,
                 BitmovinAnalyticsInternal.ErrorMessageKey: message]
     }
