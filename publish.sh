7a9b4e5532e8474105daca29b7f3be6bb9e7285f
Roland Griesser <roland.griesser@bitmovin.com>
2020-08-31 14:57:27 +0200

    Updated publish script to use ANALYTICS_GH_TOKEN from environment variables

--- publish.sh
+++ publish.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-FILE=.GITHUB_TOKEN
-if test -f "$FILE"; then
-    source $FILE
-fi
-
-if [ -z "$TOKEN" ]; then
-    echo "You need to provide the Github Access Token for the user bitAnalyticsCircleCi. This can be found in LastPass."
+if [ -z "$ANALYTICS_GH_TOKEN" ]; then
+    echo "ANALYTICS_GH_TOKEN not found in environment variables. You need to provide the Github Access Token for the user bitAnalyticsCircleCi. This can be found in LastPass."
     echo "Enter the token:"
-    read TOKEN
-    echo "TOKEN=$TOKEN" >> .GITHUB_TOKEN
+    read ANALYTICS_GH_TOKEN
+    EXPORT_TOKEN="\nexport ANALYTICS_GH_TOKEN=$ANALYTICS_GH_TOKEN"
+    if test -e ~/.bashrc; then
+        echo $EXPORT_TOKEN >> ~/.bashrc
+    fi
+    if test -e ~/.bash_profile; then
+        echo $EXPORT_TOKEN >> ~/.bash_profile
+    fi
 fi
 
 echo "Make sure to bump the version in the .podspec, README and CHANGELOG first and merge that PR into develop."
@@ -28,7 +29,7 @@ git push git@github.com:bitmovin/bitmovin-analytics-collector-ios.git main $VERS
 echo "Pushed \"main\" and \"$VERSION\" to public repo."
 
 curl \
-  -u bitAnalyticsCircleCi:$TOKEN \
+  -u bitAnalyticsCircleCi:$ANALYTICS_GH_TOKEN \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/bitmovin/bitmovin-analytics-collector-ios/releases \
