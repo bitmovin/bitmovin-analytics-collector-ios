34db0124f3337968f03c4eb849b086cd981669c4
Roland Griesser <roland.griesser@bitmovin.com>
2020-09-01 15:35:39 +0200

    Added Bitmovin api notification

--- publish.sh
+++ publish.sh
@@ -1,5 +1,32 @@
 #!/bin/sh
 
+setEnvVariable () {
+  EXPORT_TOKEN="\nexport $1=$2"
+    if test -e ~/.bashrc; then
+        echo $EXPORT_TOKEN >> ~/.bashrc
+    fi
+    if test -e ~/.bash_profile; then
+        echo $EXPORT_TOKEN >> ~/.bash_profile
+    fi
+}
+
+notifyApi () {
+  PLATFORM=$1
+  VERSION=$2
+  curl "https://api.bitmovin.com/v1/admin/analytics/releases/$PLATFORM/v$VERSION" -H "X-Api-Key:$ANALYTICS_API_RELEASE_TOKEN" -H "Content-Type:application/json" \
+    --data-binary "{
+        \"podName\": \"BitmovinAnalyticsCollector\",
+        \"gitUrl\":\"https://github.com/bitmovin/bitmovin-analytics-collector-ios.git\",
+        \"gitTag\": \"$VERSION\"
+      }" > /dev/null 2>&1
+
+  if [ $? -eq 0 ]; then
+    echo "Published Release $PLATFORM:$VERSION to Bitmovin API"
+  else
+    echo "Failed to publish release to Bitmovin API"
+  fi
+}
+
 if test -e ~/.bashrc; then
   source ~/.bashrc
 fi
@@ -11,13 +38,14 @@ if [ -z "$ANALYTICS_GH_TOKEN" ]; then
     echo "ANALYTICS_GH_TOKEN not found in environment variables. You need to provide the Github Access Token for the user bitAnalyticsCircleCi. This can be found in LastPass."
     echo "Enter the token:"
     read ANALYTICS_GH_TOKEN
-    EXPORT_TOKEN="\nexport ANALYTICS_GH_TOKEN=$ANALYTICS_GH_TOKEN"
-    if test -e ~/.bashrc; then
-        echo $EXPORT_TOKEN >> ~/.bashrc
-    fi
-    if test -e ~/.bash_profile; then
-        echo $EXPORT_TOKEN >> ~/.bash_profile
-    fi
+    setEnvVariable "ANALYTICS_GH_TOKEN" $ANALYTICS_GH_TOKEN
+fi
+
+if [ -z "$ANALYTICS_API_RELEASE_TOKEN" ]; then
+    echo "ANALYTICS_API_RELEASE_TOKEN not found in environment variables. You need to provide the API Key for the user dhi+analytics-admin-user for https://api.bitmovin.com."
+    echo "Enter the token:"
+    read ANALYTICS_API_RELEASE_TOKEN
+    setEnvVariable "ANALYTICS_API_RELEASE_TOKEN" $ANALYTICS_API_RELEASE_TOKEN
 fi
 
 echo "Make sure to bump the version in the .podspec, README and CHANGELOG first and merge that PR into develop."
@@ -43,5 +71,9 @@ curl \
   -d "{\"tag_name\":\"$VERSION\", \"name\": \"v$VERSION\", \"draft\": false}"
 
 echo "Created release in public repo."
+
+notifyApi "ios" $VERSION
+notifyApi "tvos" $VERSION
+
 echo "Don't forget to update the changelog in Contentful."
 open "https://app.contentful.com/spaces/blfijbdi3ei3/entries"
