eb7e266431355e43cbfb6e07b2649d45875145b0
Roland Griesser <roland.griesser@bitmovin.com>
2021-01-26 15:55:26 +0100

    Added BitmovinPlayerADapterTests

---
+++ Tests/BitmovinPlayerTests/BitmovinPlayerAdapterTests.swift
@@ -0,0 +1,30 @@
+import XCTest
+import BitmovinPlayer
+@testable import BitmovinAnalyticsCollector
+
+class AVPlayerAdapterTests: XCTestCase {
+    
+    func testStopMonitoringWontFailOnMultipleCalls() throws {
+        let playerConfig = PlayerConfiguration()
+        playerConfig.key = "asdf"
+        let player = Player(configuration: playerConfig)
+        let config = BitmovinAnalyticsConfig(key: "")
+        let stateMachine = StateMachine(config: config)
+        let adapter = BitmovinPlayerAdapter(player: player, config: config, stateMachine: stateMachine)
+        
+        adapter.stopMonitoring()
+        adapter.stopMonitoring()
+    }
+    
+    func testStartMonitoringWontFailOnMultipleCalls() throws {
+        let playerConfig = PlayerConfiguration()
+        playerConfig.key = "asdf"
+        let player = Player(configuration: playerConfig)
+        let config = BitmovinAnalyticsConfig(key: "")
+        let stateMachine = StateMachine(config: config)
+        let adapter = BitmovinPlayerAdapter(player: player, config: config, stateMachine: stateMachine)
+        
+        adapter.startMonitoring()
+        adapter.startMonitoring()
+    }
+}
