b26d206ce84e6a45a1afab9758aedfbd84f78709
Roland Griesser <roland.griesser@bitmovin.com>
2021-04-07 07:34:38 +0200

    Added iOS and tvOS Examples for v1

---
+++ Examples-v1/BitmovinAnalyticsCollector-v1-Example-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitmovinAnalyticsCollector-v1-Example-iOS
+//
+//  Created by Roland Grießer on 07.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
