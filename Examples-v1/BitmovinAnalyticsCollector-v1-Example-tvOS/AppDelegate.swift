c9ba1a95f094e18ab3e2c4f4e8c29d6bda9b9373
Roland Griesser <roland.griesser@bitmovin.com>
2021-04-07 08:12:28 +0200

    Added Example for tvOS

--- Examples-v1/BitmovinAnalyticsCollector-v1-Example-tvOS/AppDelegate.swift
+++ Examples-v1/BitmovinAnalyticsCollector-v1-Example-tvOS/AppDelegate.swift
@@ -1,17 +1,17 @@
 //
 //  AppDelegate.swift
-//  BitmovinAnalyticsCollector-v1-Example-tvOS
+//  BitmovinAnalyticsCollector_tvOSExample
 //
-//  Created by Roland Grießer on 07.04.21.
+//  Created by Cory Zachman on 7/5/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
 //
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -25,6 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -35,6 +36,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
 
 }
-
