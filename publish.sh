03885956abccb77be82dae8b0960bcc4ea8fec76
Bitmovin Release Automation <support@bitmovin.com>
2021-05-14 10:51:29 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -48,7 +48,7 @@ if [ -z "$ANALYTICS_API_RELEASE_TOKEN" ]; then
     setEnvVariable "ANALYTICS_API_RELEASE_TOKEN" $ANALYTICS_API_RELEASE_TOKEN
 fi
 
-echo "Make sure you have fastlane installed on your computer before releasing: brew install fastlane"
+echo "Make sure you have fastlane installed on your computer before releasing: (sudo gem install fastlane -NV)"
 echo "Make sure to bump the version in the .podspec, README and CHANGELOG first and merge that PR into develop."
 echo "Version (without leading \"v\")":
 read VERSION
