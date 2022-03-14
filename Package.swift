112ec450afd57a5bce3ec0e96ada4a5000d8a67e
Thomas Sablattnig <thomas.sablattnig@bitmovin.com>
2022-03-14 14:25:00 +0100

    test Package.swift

---
+++ Package.swift
@@ -0,0 +1,49 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BitmovinAnalyticsCollector",
+    platforms: [
+        .iOS(.v12),
+        .tvOS(.v12)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "BitmovinAnalyticsCollectorBitmovinPlayerV3",
+            targets: ["Core", "BitmovinPlayerV3"]),
+    ],
+    dependencies: [
+        .package(name: "BitmovinPlayer", url: "git@github.com:bitmovin/player-ios.git", from: "3.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Core",
+            dependencies: [],
+            path: "BitmovinAnalyticsCollector/Classes/",
+            exclude: ["BitmovinPlayer/", "AVPlayer/", "Collector/BitmovinAnalyticsCollector.h", "Collector/Info.plist", "BitmovinPlayerV3/"],
+            sources:["Collector/"]),
+        .target(
+            name: "BitmovinPlayerV3",
+            dependencies: ["BitmovinPlayer"],
+            path: "BitmovinAnalyticsCollector/Classes/",
+            exclude: ["BitmovinPlayer/", "AVPlayer/", "Collector/"],
+            sources:["BitmovinPlayerV3/"]),
+        .testTarget(
+            name: "CoreTests",
+            dependencies: ["Core"],
+            path: "BitmovinAnalyticsCollector/Tests/",
+            exclude: ["BitmovinPlayerTests/", "AVPlayerTests/", "BitmovinPlayerV3Tests/"],
+            sources:["CoreTests/"]),
+        .testTarget(
+            name: "BitmovinPlayerV3Tests",
+            dependencies: ["BitmovinPlayerV3", "BitmovinPlayer"],
+            path: "BitmovinAnalyticsCollector/Tests/",
+            exclude: ["BitmovinPlayerTests/", "AVPlayerTests/", "CoreTests/"],
+            sources:["BitmovinPlayerV3Tests/"]),
+    ]
+)
