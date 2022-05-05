a6d0c0926a1e4ec41a47ba82637489b66b357387
Thomas Sablattnig <thomas.sablattnig@bitmovin.com>
2022-05-05 11:50:05 +0200

    remove generated creation comments

--- BitmovinAnalyticsCollector/Classes/AVPlayerV2/data/AVPlayerBitrateLogProvider.swift
+++ BitmovinAnalyticsCollector/Classes/AVPlayerV2/data/AVPlayerBitrateLogProvider.swift
@@ -1,10 +1,3 @@
-//
-//  AVPlayerBitrateLogProvider.swift
-//  Pods
-//
-//  Created by Thomas Sabe on 05.05.22.
-//
-
 import Foundation
 import AVFoundation
 
--- BitmovinAnalyticsCollector/Classes/AVPlayerV2/data/BitrateLogDto.swift
+++ BitmovinAnalyticsCollector/Classes/AVPlayerV2/data/BitrateLogDto.swift
@@ -1,10 +1,3 @@
-//
-//  BitrateLogDto.swift
-//  Pods
-//
-//  Created by Thomas Sabe on 05.05.22.
-//
-
 import Foundation
 
 class BitrateLogDto {
