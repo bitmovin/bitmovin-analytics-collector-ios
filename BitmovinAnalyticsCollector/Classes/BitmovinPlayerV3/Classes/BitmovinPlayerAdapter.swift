1d427a906b0e8d5c49ea76f3b60819144d680afd
Thomas Sablattnig <thomas.sablattnig@bitmovin.com>
2023-01-05 08:11:03 +0100

    use transitionMethods and currentTime utility

--- BitmovinAnalyticsCollector/Classes/BitmovinPlayerV3/Classes/BitmovinPlayerAdapter.swift
+++ BitmovinAnalyticsCollector/Classes/BitmovinPlayerV3/Classes/BitmovinPlayerAdapter.swift
@@ -263,7 +263,7 @@ extension BitmovinPlayerAdapter: PlayerListener {
         }
         print("BitmovinAdapter: onStallStarted \(player.currentTime) isPlaying: \(player.isPlaying) isPaused: \(player.isPaused)")
         isStalling = true
-        stateMachine.transitionState(destinationState: .buffering, time: Util.timeIntervalToCMTime(_: player.currentTime))
+        stateMachine.transitionState(destinationState: .buffering, time: currentTime)
         
     }
 
@@ -279,7 +279,7 @@ extension BitmovinPlayerAdapter: PlayerListener {
         }
         print("BitmovinAdapter: onSeek \(player.currentTime) isPlaying: \(player.isPlaying) isPaused: \(player.isPaused)")
         isSeeking = true
-        stateMachine.transitionState(destinationState: .seeking, time: Util.timeIntervalToCMTime(_: player.currentTime))
+        stateMachine.seek(time: currentTime)
     }
 
     func onDownloadFinished(_ event: DownloadFinishedEvent, player: Player) {
@@ -343,7 +343,8 @@ extension BitmovinPlayerAdapter: PlayerListener {
     }
 
     func onPlaybackFinished(_ event: PlaybackFinishedEvent, player: Player) {
-        stateMachine.transitionState(destinationState: .paused, time: Util.timeIntervalToCMTime(_: player.duration))
+        let duration = Util.timeIntervalToCMTime(_: player.duration)
+        stateMachine.pause(time: duration)
         stateMachine.reset()
     }
 
@@ -361,9 +362,9 @@ extension BitmovinPlayerAdapter: PlayerListener {
             // Player reports isPlaying=true although onStallEnded has not been called yet -- still stalling
             stateMachine.transitionState(destinationState: .buffering, time: Util.timeIntervalToCMTime(_: player.currentTime))
         } else if player.isPaused {
-            stateMachine.transitionState(destinationState: .paused, time: Util.timeIntervalToCMTime(_: player.currentTime))
+            stateMachine.pause(time: currentTime)
         } else {
-            stateMachine.transitionState(destinationState: .playing, time: Util.timeIntervalToCMTime(_: player.currentTime))
+            stateMachine.playing(time: currentTime)
         }
     }
     
@@ -419,7 +420,7 @@ extension BitmovinPlayerAdapter: PlayerListener {
         guard stateMachine.state == .paused || stateMachine.state == .playing else {
             return
         }
-        stateMachine.transitionState(destinationState: .subtitlechange, time: Util.timeIntervalToCMTime(_: player.currentTime))
+        stateMachine.transitionState(destinationState: .subtitlechange, time: currentTime)
         transitionToPausedOrBufferingOrPlaying()
     }
 }
