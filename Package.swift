0f1675353e76bf6ea0913ec4ed1ab8deadec22b2
Thomas Sablattnig <thomas.sablattnig@bitmovin.com>
2023-03-30 08:34:01 +0200

    change deploy target to ios 12 (#207)
    * change deploy target to ios 12
    
    * move testing after building
    
    * run tests after build

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "BitmovinAnalytics",
     platforms: [
-        .iOS("14.2"),
-        .tvOS("14.2")
+        .iOS(.v12),
+        .tvOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
