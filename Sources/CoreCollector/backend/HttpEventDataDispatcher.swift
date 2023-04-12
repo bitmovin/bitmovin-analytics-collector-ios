ef53b3e7bf5a5161848051e694dd13e4a045ffa9
Mario Graf <mario.graf@bitmovin.com>
2023-04-12 16:22:50 +0200

    Dispatch calls to completion handler to main queue. Introduces isEnabled flag for the sake of completeness

--- Sources/CoreCollector/backend/HttpEventDataDispatcher.swift
+++ Sources/CoreCollector/backend/HttpEventDataDispatcher.swift
@@ -3,9 +3,9 @@ import Foundation
 internal class HttpEventDataDispatcher: EventDataDispatcher, CallbackEventDataDispatcher {
     private let logger = _AnalyticsLogger(className: "HttpEventDataDispatcher")
     private let httpClient: HttpClient
-
     private let analyticsBackendUrl: String
     private let adAnalyticsBackendUrl: String
+    private var isEnabled: Bool = true
 
     init(httpClient: HttpClient) {
         self.httpClient = httpClient
@@ -15,14 +15,20 @@ internal class HttpEventDataDispatcher: EventDataDispatcher, CallbackEventDataDi
     }
 
     func add(_ eventData: EventData) {
+        guard isEnabled else { return }
+
         add(eventData) { _ in }
     }
 
     func addAd(_ adEventData: AdEventData) {
+        guard isEnabled else { return }
+
         addAd(adEventData) { _ in }
     }
 
     func add(_ eventData: EventData, completionHandler: @escaping (HttpDispatchResult) -> Void) {
+        guard isEnabled else { return }
+
         let json = Util.toJson(object: eventData)
         logger.d("send payload: \(json.replacingOccurrences(of: ",", with: "\n\t"))")
 
@@ -30,11 +36,15 @@ internal class HttpEventDataDispatcher: EventDataDispatcher, CallbackEventDataDi
             urlString: analyticsBackendUrl,
             json: eventData.jsonString()
         ) { data, response, error in
-            completionHandler(HttpDispatchResult.from(data: data, response: response, error: error))
+            DispatchQueue.main.async {
+                completionHandler(HttpDispatchResult.from(data: data, response: response, error: error))
+            }
         }
     }
 
     func addAd(_ adEventData: AdEventData, completionHandler: @escaping (HttpDispatchResult) -> Void) {
+        guard isEnabled else { return }
+
         let json = Util.toJson(object: adEventData)
         logger.d("send Ad payload: \(json)")
 
@@ -42,13 +52,15 @@ internal class HttpEventDataDispatcher: EventDataDispatcher, CallbackEventDataDi
             urlString: adAnalyticsBackendUrl,
             json: json
         ) { data, response, error in
-            completionHandler(HttpDispatchResult.from(data: data, response: response, error: error))
+            DispatchQueue.main.async {
+                completionHandler(HttpDispatchResult.from(data: data, response: response, error: error))
+            }
         }
     }
 
     func disable() {
+        isEnabled = false
     }
 
-    func resetSourceState() {
-    }
+    func resetSourceState() { /* no-op */ }
 }
