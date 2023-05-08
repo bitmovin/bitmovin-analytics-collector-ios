02d5570bbe2c17e46ec85dafaa609abe5b5dd9e4
Mario Graf <mario.graf@bitmovin.com>
2023-05-08 21:20:35 +0200

    Only clean up database when needed, as it is an expensive operation

--- Sources/CoreCollector/backend/offline/PersistentEventDataQueue.swift
+++ Sources/CoreCollector/backend/offline/PersistentEventDataQueue.swift
@@ -20,25 +20,49 @@ internal class PersistentEventDataQueue {
     func add(_ eventData: EventData) async {
         guard eventData.sequenceNumber <= maxSequenceNumber else { return }
 
-        await cleanUpDatabase()
+        if await eventDataQueue.count >= maxEntries {
+            await cleanUpDatabase()
+        }
+
         await eventDataQueue.add(entry: eventData)
         logger.d("Added event data to queue")
     }
 
     func addAd(_ adEventData: AdEventData) async {
-        await cleanUpDatabase()
+        if await eventDataQueue.count >= maxEntries {
+            await cleanUpDatabase()
+        }
+
         await adEventDataQueue.add(entry: adEventData)
         logger.d("Added ad event data to queue")
     }
 
     func removeFirst() async -> EventData? {
-        await cleanUpDatabase()
-        return await eventDataQueue.removeFirst()
+        guard let eventData = await eventDataQueue.removeFirst() else { return nil }
+
+        if eventData.age <= maxEntryAge {
+            return eventData
+        }
+
+        logger.d("Entry exceeding max age found, discarding old sessions and fetching next")
+        await cleanUpDatabase(including: eventData.impressionId)
+
+        return await removeFirst()
     }
 
     func removeFirstAd() async -> AdEventData? {
-        await cleanUpDatabase()
-        return await adEventDataQueue.removeFirst()
+        guard let adEventData = await adEventDataQueue.removeFirst() else { return nil }
+
+        if adEventData.age <= maxEntryAge {
+            return adEventData
+        }
+
+        logger.d("Entry exceeding max age found, discarding old sessions and fetching next")
+        if let impressionId = adEventData.videoImpressionId {
+            await cleanUpDatabase(including: impressionId)
+        }
+
+        return await removeFirstAd()
     }
 
     func removeAll() async {
@@ -48,8 +72,15 @@ internal class PersistentEventDataQueue {
 }
 
 private extension PersistentEventDataQueue {
-    func cleanUpDatabase() async {
-        await purgeEntries(for: await findOldSessionsToPurge())
+    func cleanUpDatabase(including impressionId: String? = nil) async {
+        logger.d("Cleaning up database")
+
+        var impressionIdsToPurge = await findOldSessionsToPurge()
+        if let impressionId {
+            impressionIdsToPurge.insert(impressionId)
+        }
+
+        await purgeEntries(for: impressionIdsToPurge)
 
         while await eventDataQueue.count >= maxEntries {
             guard let entryToPurge = await eventDataQueue.removeFirst() else { break }
@@ -74,6 +105,10 @@ private extension PersistentEventDataQueue {
     }
 
     func purgeEntries(for impressionIds: Set<String>) async {
+        guard !impressionIds.isEmpty else { return }
+
+        logger.d("Purging entries for \(impressionIds.count) impression IDs")
+
         await eventDataQueue.removeAll { eventData in
             return impressionIds.contains(eventData.impressionId)
         }
