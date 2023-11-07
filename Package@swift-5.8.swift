744a426cc989852b0103cdd57914fd0239ee0e49
Bitmovin Release Automation <support@bitmovin.com>
2023-11-07 13:36:14 +0100

    Release 3.2.0-a.4

--- Package@swift-5.8.swift
+++ Package@swift-5.8.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,7 +8,6 @@ let package = Package(
     platforms: [
         .iOS(.v14),
         .tvOS(.v14),
-        .visionOS(.v1),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -26,7 +25,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/bitmovin/player-ios-core.git", exact: "3.48.0-rc.1"),
+        .package(url: "https://github.com/bitmovin/player-ios-core.git", from: "3.41.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -44,8 +43,8 @@ let package = Package(
         .target(
             name: "AVFoundationCollectorTarget",
             dependencies: [
-                .target(name: "CoreCollector", condition: .when(platforms: [.iOS, .tvOS])),
-                .target(name: "AVFoundationCollector", condition: .when(platforms: [.iOS, .tvOS])),
+                "CoreCollector",
+                "AVFoundationCollector",
             ]
         ),
         .binaryTarget(
